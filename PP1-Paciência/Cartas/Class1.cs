a9cfe64be0adff10ec0fc779f275b22e330db412
Davi Dias <davidiasdavila3004@gmail.com>
2025-06-30 19:45:59 -0300

    Adição e Inserção das Pilhas

---
+++ PP1-Paciência/Cartas/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP1_Paciência.Cartas
+{
+    class Class1
+    {
+    }
+}
